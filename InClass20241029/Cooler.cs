2fa73d8aa9d2ece7a29041e33c31314bbff71dc1
Nathan Sharpe <sharpenn@mail.uc.edu>
2024-10-29 12:19:42 -0400

    Add project files.

---
+++ InClass20241029/Cooler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass20241029
+{
+    /// <summary>
+    /// This class models the cooler found in our classroom
+    /// </summary>
+    internal class Cooler
+    {
+        private String _color;  // Properties by default are private. Private properties are prefixed with an _ by convention.
+        public string Color
+        { // Provide access to the private property. Always add getter and setter for private properties. Getter and Setter are public.
+            get { return _color; }
+            set
+            {
+                if (value.Length > 0) // Color cannot be null 
+                {
+                    _color = value;
+                }
+            }
+        }
+
+        private int _cost;
+        public int Cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (value >= 0) // The cost cannot be negative
+                {
+                    _cost = value;
+                }
+            }
+        
+        }
+    }
+}
---
+++ InClass20241029/Program.cs
@@ -0,0 +1,28 @@
+﻿// Documentation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass20241029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare an object of type cooler
+            Cooler myCooler;
+            myCooler = new Cooler(); // Instantiate the object 
+            myCooler.Color = "Dirty Gray"; // Invokes the setter 
+            Console.WriteLine(myCooler.Color); // Invoke the getter
+            myCooler.Color = ""; // Empty string value
+            Console.WriteLine("New cooler color is " + myCooler.Color);
+            myCooler.Cost = 20;
+            Console.WriteLine("The cooler cost is " + myCooler.Cost);
+            myCooler.Cost = -1;
+            Console.WriteLine("The cooler cost is " + myCooler.Cost);
+        } 
+    }
+}
